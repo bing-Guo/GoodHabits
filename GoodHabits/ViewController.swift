a9796d5241bece126ee50b10acef3cdc66ed1646
Bing <bindaguo@gmail.com>
2019-12-30 23:47:27 +0800

    Initial Commit

---
+++ GoodHabits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodHabits
+//
+//  Created by Bing Guo on 2019/12/30.
+//  Copyright © 2019 Bing Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GoodHabitsTests/GoodHabitsTests.swift
@@ -0,0 +1,34 @@
+//
+//  GoodHabitsTests.swift
+//  GoodHabitsTests
+//
+//  Created by Bing Guo on 2019/12/30.
+//  Copyright © 2019 Bing Guo. All rights reserved.
+//
+
+import XCTest
+@testable import GoodHabits
+
+class GoodHabitsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
